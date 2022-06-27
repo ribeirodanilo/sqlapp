730ac3a7a933765c883cfba9fc62b0ce9a939d1e
Danilo Ribeiro <ribeiro.danilo@gmail.com>
2022-06-26 23:02:39 -0300

    novas versoes kubernets

--- sqlapp/Services/ProductService.cs
+++ sqlapp/Services/ProductService.cs
@@ -1,5 +1,5 @@
-﻿using System.Data.SqlClient;
-using sqlapp.Models;
+﻿using sqlapp.Models;
+using MySql.Data.MySqlClient;
 
 namespace sqlapp.Services;
 
@@ -11,30 +11,43 @@ public class ProductService
     private static string db_password = "Pa55w.rdPa55w.rd";
     private static string db_database = "appdb";
 
-    private readonly IConfiguration _configuration;
+    //private readonly IConfiguration _configuration;
 
-    public ProductService(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
+    //public ProductService(IConfiguration configuration)
+    //{
+    //  _configuration = configuration;
+    //}
 
-    private SqlConnection GetConnection()
+    private MySqlConnection GetConnection()
     {
 
         // Utilizar este codigo para ConnectionString:
-        return new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
+        //SQLConnection = "Server=tcp:appserver-1.database.windows.net,1433;Initial Catalog=appdb;Persist Security Info=False;User ID=administratordr;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+        //return new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
 
-        var _builder = new SqlConnectionStringBuilder();
-        _builder.DataSource = db_source;
-        _builder.UserID = db_user; 
-        _builder.Password = db_password;    
-        _builder.InitialCatalog = db_database;
-        return new SqlConnection(_builder.ConnectionString);
+        /* Utilizar este codigo para SQLServer
+             var _builder = new SqlConnectionStringBuilder();
+            _builder.Server = db_source;
+            _builder.UserID = db_user; 
+            _builder.Password = db_password;    
+            _builder.Database = db_database;
+            return new SqlConnection(_builder.ConnectionString);          
+         */
+
+        // Utilizar este codigo para MySQL como servico Azure
+        // string connectionString = "Server=mysqlserver-302.mysql.database.azure.com; Port=3306; Database=appdb; Uid=administratordr@mysqlserver-302; Pwd=Pa55w.rdPa55w.rd; SslMode=Preferred;";
+
+        // utilizar este string como container
+        //string connectionString = "Server=20.243.68.245; Port=3306; Database=appdb; Uid=root; Pwd=Pa55w.rdPa55w.rd; SslMode=Preferred;";
+
+        // utilizar este string como Container Group
+        string connectionString = "Server=localhost; Port=3306; Database=appdb; Uid=root; Pwd=Pa55w.rdPa55w.rd; SslMode=Preferred;";
+        return new MySqlConnection(connectionString);
     }
 
     public List<Product> GetProducts()
     {
-        SqlConnection conn = GetConnection();
+        MySqlConnection conn = GetConnection();
 
         List<Product> _produtct_list = new List<Product>();
 
@@ -42,9 +55,9 @@ public class ProductService
 
         conn.Open();
 
-        SqlCommand cmd = new SqlCommand(statement, conn);
+        MySqlCommand cmd = new MySqlCommand(statement, conn);
 
-        using (SqlDataReader reader = cmd.ExecuteReader())
+        using (MySqlDataReader reader = cmd.ExecuteReader())
         {
             while(reader.Read())
             {
